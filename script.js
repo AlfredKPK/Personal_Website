974480f4bbec2d2fa26518855dec7597e5da05e7
AlfredKPK <alfredkong4school@gmail.com>
2024-02-25 21:17:13 +0800

    Done

---
+++ script.js
@@ -0,0 +1,10 @@
+
+<script type="text/javascript">
+  function showDate() {
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; //months are zero based
+    var curr_year = d.getFullYear();
+    document.write(curr_date + "-" + curr_month + "-" + curr_year);
+  }
+</script>
